59af9fbdac1bd59fd59ecf6a96eec8fdce856825
musejournal <3159546973@qq.com>
2024-11-20 12:56:27 +0800

    Site updated: 2024-11-20 12:56:27

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732073626615";
+const VERSION = "1732078580167";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
